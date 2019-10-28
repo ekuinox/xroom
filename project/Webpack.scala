a9fc4ecac4ca4ef16fdc71e233b8e0e9d51cdfe5
ekuinox <depkey@me.com>
2019-10-29 00:55:04 +0900

    webpackが通って欲しいニャン...

---
+++ project/Webpack.scala
@@ -0,0 +1,14 @@
+import play.sbt.PlayRunHook
+import sbt._
+import scala.sys.process.Process
+
+object Webpack {
+  def apply(base: File): PlayRunHook = {
+    object WebpackProcess extends PlayRunHook {
+      override def beforeStarted(): Unit = {
+        Process("yarn webpack --watch", new File(base.getPath + "/front")).run
+      }
+    }
+    WebpackProcess
+  }
+}
