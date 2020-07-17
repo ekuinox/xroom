31ba0ef3cfed2830da51a459325b5c9e00c72337
ekuinox <depkey@me.com>
2020-07-18 01:38:37 +0900

    add comments

--- app/room/messages/UpdatePenMessage.scala
+++ app/room/messages/UpdatePenMessage.scala
@@ -5,6 +5,11 @@ import room.canvas.Pen
 
 case class UpdatePenData(username: String, pen: Pen)
 
+/**
+ * Server <=> Client
+ * 新しいPenの状態を送る
+ * @param data メッセージ本体
+ */
 case class UpdatePenMessage(override val data: UpdatePenData) extends Message[UpdatePenData, _]("UpdatePen", data)
 
 object UpdatePenMessage {
